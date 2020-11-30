8303dc28b69a0cce091c246f1d5dcf1b8781a84b
George Wei <georgewei369@yahoo.com>
2020-11-30 15:33:31 -0500

    Added comments and changed testing procedure to reflect changes of encoder

--- img_caption/baseline/test.py
+++ img_caption/baseline/test.py
@@ -17,74 +17,86 @@ from tqdm import tqdm
 
 
 def clean_sentence(output, data_loader):
+
+    # Stores words corresponding to indices
     words_sequence = []
 
+    # Iterates over each word in the output
     for i in output:
+
+        # Skip <EOS> tokens
         if i == 1:
             continue
+
+        # Otherwise add word to sentence
         words_sequence.append(data_loader.dataset.vocab.itos[i])
 
+    # Skip end tokens
     words_sequence = words_sequence[1: -1]
+    # Join sentence
     sentence = " ".join(words_sequence)
+    # Capitalize sentence
     sentence = sentence.capitalize()
 
     return sentence
 
 
 def build_json(encoder, decoder, data_loader, device, filename):
+
+    # Set both encoder and decoder to evaluation mode
     encoder.eval()
     decoder.eval()
 
+    # Stores all of the small dictionaries
     result = list()
 
+    # No need for gradient
     with torch.no_grad():
+
+        # Iterates over each batch
         for batch_idx, (img_id, image) in tqdm(enumerate(data_loader)):
+
+            # Change to CUDA if possible
             image = image.to(device)
-
+            # Add a dimension for timestep
             features = encoder(image).unsqueeze(1)
+            # Sample the sentence from decoder
             output = decoder.sample(features)
+            # Clean up sentence so that it is human readable
             sentence = clean_sentence(output, data_loader)
-
+            # Add resulting caption with its image id
             result.append({"image_id": img_id.item(), "captions": sentence})
 
+    # Dump into specified file
     with open(filename + ".json", "w") as f:
         json.dump(result, f)
 
 
-# def get_prediction(encoder, decoder, data_loader):
-#     orig_image, image = next(iter(data_loader))
-#     plt.imshow(np.squeeze(orig_image))
-#     plt.title("Sample Image")
-#     plt.show()
-#     image = image.to(device)
-#     features = encoder(image).unsqueeze(1)
-#     output = decoder.sample(features)
-#     sentence = clean_sentence(output)
-#     print(sentence)
-
-
+# Hyperparameters
 batch_size = 1
 vocab_threshold = 5
 vocab_from_file = True
-embed_size = 300
+embed_size = 400
 hidden_size = 512
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-
+# Transformations
 transform_test = transforms.Compose([
-    transforms.Resize(256),
-    transforms.CenterCrop(224),
+    transforms.Resize(328),
+    transforms.CenterCrop(299),
     transforms.ToTensor(),
     transforms.Normalize((0.485, 0.456, 0.406),
                          (0.229, 0.224, 0.225))
 ])
 
-# val_loader = get_loader(transform=transform_test,
-#                         mode="valid",
-#                         batch_size=batch_size,
-#                         vocab_threshold=vocab_threshold,
-#                         vocab_from_file=vocab_from_file)
+# Validation loader
+val_loader = get_loader(transform=transform_test,
+                        mode="valid",
+                        batch_size=batch_size,
+                        vocab_threshold=vocab_threshold,
+                        vocab_from_file=vocab_from_file)
 
+# Test loader
 test_loader = get_loader(transform=transform_test,
                          mode="test")
 
@@ -99,14 +111,14 @@ decoder = DecoderRNN(embed_size, hidden_size, vocab_size)
 encoder.to(device)
 decoder.to(device)
 
-encoder_file = "encoder-2.pkl"
-decoder_file = "decoder-2.pkl"
+# Saved checkpoints
+encoder_file = "encoder-3.pkl"
+decoder_file = "decoder-3.pkl"
 
+# Load checkpoints
 encoder.load_state_dict(torch.load(os.path.join("./models", encoder_file)))
 decoder.load_state_dict(torch.load(os.path.join("./models", decoder_file)))
 
-encoder.to(device)
-decoder.to(device)
-
-# build_json(encoder, decoder, val_loader, device, "validation_preds")
-build_json(encoder, decoder, test_loader, device, "test_preds")
+# Building the output jsons for server evaluation
+build_json(encoder, decoder, val_loader, device, "captions_val2014_baseline_results")
+build_json(encoder, decoder, test_loader, device, "captions_test2014_baseline_results")
