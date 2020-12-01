6363a6143f25c8e398d566eb13bc7553d4ac17d4
George Wei <georgewei369@yahoo.com>
2020-11-30 20:01:31 -0500

    Fixed torch installation bug

--- img_caption/baseline/setup.sh
+++ img_caption/baseline/setup.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-pip install torch
-pip install torchvision
-pip install numpy
-pip install pycocotools
-pip install nltk
+pip install --user torch===1.7.0 torchvision===0.8.1 torchaudio===0.7.0 -f https://download.pytorch.org/whl/torch_stable.html
+pip install --user numpy
+pip install --user pycocotools
+pip install --user nltk
 python3 -c "import nltk; nltk.download('punkt')"
