d93b9063777eef998bb261bc881323885bc32c60
George Wei <georgewei369@yahoo.com>
2020-11-30 20:13:31 -0500

    Removed download for torch

--- img_caption/baseline/setup.sh
+++ img_caption/baseline/setup.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-pip install --user torch===1.7.0 torchvision===0.8.1 torchaudio===0.7.0 -f https://download.pytorch.org/whl/torch_stable.html
 pip install --user numpy
 pip install --user pycocotools
 pip install --user nltk
